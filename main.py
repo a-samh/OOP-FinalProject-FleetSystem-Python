8726e3a8d5d7cf2dbe70e711686cb3a40db8d63b
ryazdi-lab <r.yazdi@gu.ac.ir>
2026-07-03 11:21:37 +0330

    Revert "Feature/student- 40414162126"

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from models.vehicle import Vehicle
-from models.car import Car
-from models.truck import Truck
-from models.motorcycle import Motorcycle
 
-def run_fleet_simulation(fleet_: list[Vehicle]) -> None:
-    for v in fleet_:
+def run_fleet_simulation(fleet: list[Vehicle]) -> None:
+    for v in fleet:
         v.display_info()
         print(f"هزینه روزانه: {v.calculate_daily_cost():.2f}")
         v.perform_maintenance()
@@ -13,8 +10,7 @@ def run_fleet_simulation(fleet_: list[Vehicle]) -> None:
 if __name__ == "__main__":
     # دانشجویان اینجا اشیاء concrete را می‌سازند و به لیست اضافه می‌کنند
     fleet: list[Vehicle] = [
-        Car("DA-655-QL", "Tesla", 2024, 230.0),
-        Truck("HCM-6223", "DAF", 2019, 420.0),
-        Motorcycle("4559 RW", "Aprilia", 2025, 110.0)
+        # Car("12345", "Toyota", 2020, 150.0),
+        # Truck("TRK-99", "Volvo", 2018, 300.0),
     ]
     run_fleet_simulation(fleet)
